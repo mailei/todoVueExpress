0975accea0c1e261bc89431431478b1ab9bc019d
iwazaki.y <memorycross@gmail.com>
2019-02-27 19:56:41 +0900

    fix eslint rule

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -21,6 +21,8 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    "semi": 'off',
+    'space-before-function-paren': 'off',
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
--- client/src/modules/todoRequest.js
+++ client/src/modules/todoRequest.js
@@ -2,34 +2,32 @@ import axios from 'axios';
 
 const url = 'http://localhost:5000/api/posts/';
 
-class todoRequest{
-  
-  static getTodo(){
-    return new Promise(async (resulve,reject)=>{
-      try{
-        const res=await axios.get(url);
-        const data=res.data;
-        resulve(
-          data.map(todo=>({
+class todoRequest {
+  static getTodo() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await axios.get(url);
+        const data = res.data;
+        resolve(
+          data.map(todo => ({
             ...todo,
             createAt: new Date(todo.createAt)
-            })
+          })
           ));
-      }catch(error){
+      } catch (error) {
         reject(error);
       }
     });
   }
 
-  static createTodo(text){
-    return axios.post(url,{
+  static createTodo(text) {
+    return axios.post(url, {
       text
     });
   }
 
-  static deleteTodo(id){
+  static deleteTodo(id) {
     return axios.delete(`${url}${id}`);
   }
-
 }
 export default todoRequest;
